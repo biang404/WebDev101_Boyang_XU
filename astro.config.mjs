d9fa3f07a9e7c5886a8ebabf9993e2ed2412d436
biang404 <108710728+biang404@users.noreply.github.com>
2025-05-07 17:43:47 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zhiying318.github.io',
-    base: '/WebDev101/', // 重要！适配 GitHub Pages 子路径
+    site: 'https://biang404.github.io',
+    base: '/WebDev101_Boyang_XU/', // 重要！适配 GitHub Pages 子路径
 });
